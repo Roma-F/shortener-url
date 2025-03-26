a9eb4324e838359279865c55e332ee68ee0ca1b5
Роман Фасхутдинов <vizionssmith@gmail.com>
2025-03-26 13:21:28 +0800

    fix/iter5

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"strings"
 )
 
 type ServerOption struct {
@@ -11,16 +13,30 @@ type ServerOption struct {
 }
 
 func NewServerOption() *ServerOption {
-	opts := &ServerOption{}
-	flag.StringVar(&opts.RunAddr, "a", ":8080", "address and port to run server")
-	flag.StringVar(&opts.ShortURLAddr, "b", "http://localhost:8080", "base address of the resulting shortened URL")
+	defaultRunAddr := ":8080"
+	defaultBaseURL := "http://localhost:8080"
 
-	if v, ok := os.LookupEnv("SERVER_ADDRESS"); ok {
-		opts.RunAddr = v
+	flag.StringVar(&defaultRunAddr, "server-port", defaultRunAddr, "address and port to run server")
+	flag.StringVar(&defaultBaseURL, "base-url", defaultBaseURL, "base address for resulting shortened URL")
+	flag.Parse()
+
+	opts := &ServerOption{
+		RunAddr:      defaultRunAddr,
+		ShortURLAddr: defaultBaseURL,
+	}
+
+	if v, ok := os.LookupEnv("SERVER_PORT"); ok {
+		if !strings.Contains(v, ":") {
+			opts.RunAddr = ":" + v
+		} else {
+			opts.RunAddr = v
+		}
 	}
 	if v, ok := os.LookupEnv("BASE_URL"); ok {
 		opts.ShortURLAddr = v
 	}
 
+	fmt.Println("Server will run on", opts.RunAddr)
+	fmt.Println("Base URL is", opts.ShortURLAddr)
 	return opts
 }
