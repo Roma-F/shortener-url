c9d9cbbbff7a9f4a63a0f4aca3f37007c6bd61a2
Роман Фасхутдинов <vizionssmith@gmail.com>
2025-04-15 15:00:43 +0800

    iter7

--- internal/app/transport/handler/url_handler.go
+++ internal/app/transport/handler/url_handler.go
@@ -1,12 +1,16 @@
 package handler
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 	"strconv"
 	"strings"
 
+	"github.com/Roma-F/shortener-url/internal/app/logger"
+	"github.com/Roma-F/shortener-url/internal/app/models"
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 )
 
 type URLShortener interface {
@@ -22,7 +26,7 @@ func NewURLHandler(svc URLShortener) *URLHandler {
 	return &URLHandler{service: svc}
 }
 
-func (h *URLHandler) ShortenURL(w http.ResponseWriter, r *http.Request) {
+func (h *URLHandler) ShortenURLTextPlain(w http.ResponseWriter, r *http.Request) {
 	if !strings.HasPrefix(r.Header.Get("Content-Type"), "text/plain") {
 		http.Error(w, "Content-Type most be text/plain", http.StatusBadRequest)
 		return
@@ -46,6 +50,43 @@ func (h *URLHandler) ShortenURL(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Length", strconv.Itoa(len(shortURL)))
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(shortURL))
+}
+
+func (h *URLHandler) ShortenURLJSON(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.Header.Get("Content-Type"), "application/json") {
+		http.Error(w, "Content-Type most be application/json", http.StatusBadRequest)
+		return
+	}
+
+	var req models.ShortenURLReq
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		logger.Sugar.Debug("cannot decode request JSON body", zap.Error(err))
+		http.Error(w, "Invalid JSON payload", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	shortURL, err := h.service.GenerateShortURL(req.Url)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	resp := models.ShortenURLResp{
+		Result: shortURL,
+	}
+
+	jsonData, err := json.MarshalIndent(resp, "", "   ")
+	if err != nil {
+		http.Error(w, "Error creating JSON response: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Length", strconv.Itoa(len(jsonData)))
+	w.WriteHeader(http.StatusCreated)
+	w.Write(jsonData)
+
 }
 
 func (h *URLHandler) GetMainURL(w http.ResponseWriter, r *http.Request) {
--- internal/app/transport/handler/url_handler_test.go
+++ internal/app/transport/handler/url_handler_test.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 	"testing"
 
 	"github.com/Roma-F/shortener-url/internal/app/config"
+	"github.com/Roma-F/shortener-url/internal/app/models"
 	"github.com/Roma-F/shortener-url/internal/app/service"
 	"github.com/Roma-F/shortener-url/internal/app/storage"
 	"github.com/stretchr/testify/assert"
@@ -24,7 +26,7 @@ func setupHandler() *URLHandler {
 	return NewURLHandler(svc)
 }
 
-func TestURLHandler_ShortenURL_Success(t *testing.T) {
+func TestURLHandler_ShortenURLTextPlain_Success(t *testing.T) {
 	handler := setupHandler()
 
 	originalURL := "https://example.com"
@@ -33,7 +35,7 @@ func TestURLHandler_ShortenURL_Success(t *testing.T) {
 	req.Host = "example.com"
 
 	rr := httptest.NewRecorder()
-	handler.ShortenURL(rr, req)
+	handler.ShortenURLTextPlain(rr, req)
 
 	resp := rr.Result()
 	defer resp.Body.Close()
@@ -68,7 +70,7 @@ func TestURLHandler_ShortenURL_MethodNotAllowed(t *testing.T) {
 	req.Host = "example.com"
 	rr := httptest.NewRecorder()
 
-	handler.ShortenURL(rr, req)
+	handler.ShortenURLTextPlain(rr, req)
 
 	resp := rr.Result()
 	defer resp.Body.Close()
@@ -82,7 +84,7 @@ func TestURLHandler_ShortenURL_InvalidContentType(t *testing.T) {
 	req.Host = "example.com"
 	rr := httptest.NewRecorder()
 
-	handler.ShortenURL(rr, req)
+	handler.ShortenURLTextPlain(rr, req)
 
 	resp := rr.Result()
 	defer resp.Body.Close()
@@ -124,7 +126,7 @@ func TestURLHandler_GetMainURL_Success(t *testing.T) {
 	reqShorten.Header.Set("Content-Type", "text/plain")
 	reqShorten.Host = "example.com"
 	rrShorten := httptest.NewRecorder()
-	handler.ShortenURL(rrShorten, reqShorten)
+	handler.ShortenURLTextPlain(rrShorten, reqShorten)
 	respShorten := rrShorten.Result()
 	defer respShorten.Body.Close()
 	body, err := io.ReadAll(respShorten.Body)
@@ -138,4 +140,31 @@ func TestURLHandler_GetMainURL_Success(t *testing.T) {
 	handler.GetMainURL(rrGet, reqGet)
 	respGet := rrGet.Result()
 	defer respGet.Body.Close()
+}
+
+func TestURLHandler_ShortenURLJSON_Success(t *testing.T) {
+	handler := setupHandler()
+
+	payload := `{"url": "https://example.com"}`
+	req := httptest.NewRequest(http.MethodPost, "/api/shorten", strings.NewReader(payload))
+	req.Header.Set("Content-Type", "application/json")
+	req.Host = "example.com"
+
+	rr := httptest.NewRecorder()
+	handler.ShortenURLJSON(rr, req)
+
+	resp := rr.Result()
+	defer resp.Body.Close()
+
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+
+	body, err := io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+
+	var jsonResp models.ShortenURLResp
+	err = json.Unmarshal(body, &jsonResp)
+	assert.NoError(t, err)
+
+	expectedPrefix := "http://localhost:8080/"
+	assert.True(t, strings.HasPrefix(jsonResp.Result, expectedPrefix), "short URL should start with %s", expectedPrefix)
 }
