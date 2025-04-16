b780ce33448bf0e6b9564c760eaf23269c4c11fe
Роман Фасхутдинов <vizionssmith@gmail.com>
2025-04-16 23:12:01 +0800

    fix

--- internal/app/transport/middleware/gzip.go
+++ internal/app/transport/middleware/gzip.go
@@ -1,28 +1,40 @@
 package middleware
 
 import (
+	"bytes"
 	"compress/gzip"
 	"io"
 	"net/http"
 	"strings"
 )
 
-type gzipResponseWriter struct {
-	http.ResponseWriter
-	writer *gzip.Writer
+type responseBuffer struct {
+	buf         *bytes.Buffer
+	statusCode  int
+	header      http.Header
+	wroteHeader bool
 }
 
-func (w *gzipResponseWriter) Write(b []byte) (int, error) {
-	return w.writer.Write(b)
-}
-
-func (w *gzipResponseWriter) WriteHeader(statusCode int) {
-	contentType := w.Header().Get("Content-Type")
-	if strings.Contains(contentType, "application/json") ||
-		strings.Contains(contentType, "text/html") {
-		w.Header().Set("Content-Encoding", "gzip")
+func newResponseBuffer() *responseBuffer {
+	return &responseBuffer{
+		buf:    new(bytes.Buffer),
+		header: make(http.Header),
 	}
-	w.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (rb *responseBuffer) Header() http.Header {
+	return rb.header
+}
+
+func (rb *responseBuffer) Write(p []byte) (int, error) {
+	return rb.buf.Write(p)
+}
+
+func (rb *responseBuffer) WriteHeader(statusCode int) {
+	if !rb.wroteHeader {
+		rb.statusCode = statusCode
+		rb.wroteHeader = true
+	}
 }
 
 func WithGzip(next http.Handler) http.Handler {
@@ -30,7 +42,7 @@ func WithGzip(next http.Handler) http.Handler {
 		if r.Header.Get("Content-Encoding") == "gzip" {
 			gzReader, err := gzip.NewReader(r.Body)
 			if err != nil {
-				http.Error(w, "Failed to unpack request body", http.StatusBadRequest)
+				http.Error(w, "Unable to decompress request", http.StatusBadRequest)
 				return
 			}
 			defer gzReader.Close()
@@ -43,16 +55,40 @@ func WithGzip(next http.Handler) http.Handler {
 			return
 		}
 
-		gzWriter := gzip.NewWriter(w)
-		defer gzWriter.Close()
-
-		wrappedWriter := &gzipResponseWriter{
-			ResponseWriter: w,
-			writer:         gzWriter,
+		rwBuffer := newResponseBuffer()
+		for k, v := range w.Header() {
+			for _, vv := range v {
+				rwBuffer.header.Add(k, vv)
+			}
 		}
 
-		w.Header().Add("Vary", "Accept-Encoding")
+		next.ServeHTTP(rwBuffer, r)
 
-		next.ServeHTTP(wrappedWriter, r)
+		contentType := rwBuffer.header.Get("Content-Type")
+		if strings.Contains(contentType, "application/json") || strings.Contains(contentType, "text/html") {
+			w.Header().Set("Vary", "Accept-Encoding")
+			w.Header().Set("Content-Encoding", "gzip")
+
+			w.Header().Del("Content-Length")
+			for k, v := range rwBuffer.header {
+				if strings.ToLower(k) == "content-length" {
+					continue
+				}
+				w.Header()[k] = v
+			}
+			w.WriteHeader(rwBuffer.statusCode)
+			gzWriter := gzip.NewWriter(w)
+			defer gzWriter.Close()
+			_, err := gzWriter.Write(rwBuffer.buf.Bytes())
+			if err != nil {
+			}
+		} else {
+
+			for k, v := range rwBuffer.header {
+				w.Header()[k] = v
+			}
+			w.WriteHeader(rwBuffer.statusCode)
+			_, _ = w.Write(rwBuffer.buf.Bytes())
+		}
 	})
 }
